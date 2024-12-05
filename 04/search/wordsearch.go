d0bd9ac91241f69ba34f54afc58241f86d4b94f9
RobUrsem <rursem@e2g.com>
2024-12-04 23:34:11 -0700

    feat: find string horizontal forward and backward, as string

--- 04/search/wordsearch.go
+++ 04/search/wordsearch.go
@@ -1,13 +1,43 @@
 package search
 
-import "strings"
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+func reverse(s string) string {
+	size := len(s)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(s[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
+}
+
+func searchForward(needle, haystack string) int {
+	count := 0
+	offset := 0
+	for {
+		index := strings.Index(haystack[offset:], needle)
+		if index == -1 {
+			break
+		}
+
+		count++
+		offset = offset + index + 1
+	}
+
+	return count
+}
 
 func SearchForWord(needle string, haystack []string) int {
+	rneedle := reverse(needle)
 	count := 0
 	for _, line := range haystack {
-		if strings.HasPrefix(line, needle) {
-			count++
-		}
+		count += searchForward(needle, line)
+		count += searchForward(rneedle, line)
 	}
 	return count
 }
