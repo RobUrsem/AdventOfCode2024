8273a32210b25227be2f0388879d5dc5e4221a8e
RobUrsem <rursem@e2g.com>
2024-12-06 23:43:58 -0700

    Day 04a: gold star

--- 04/search/wordsearch.go
+++ 04/search/wordsearch.go
@@ -16,10 +16,6 @@ func reverse(s string) string {
 	return string(buf)
 }
 
-func stringToArray(s string) []rune {
-	return []rune(s)
-}
-
 func searchHorizontal(needle, haystack string) int {
 	count := 0
 	offset := 0
@@ -36,7 +32,7 @@ func searchHorizontal(needle, haystack string) int {
 	return count
 }
 
-func searchVertical(needle []rune, haystack []string) int {
+func searchVertical(needle string, haystack []string) int {
 	numRows := len(haystack)
 	numColumns := len(haystack[0])
 	needleLength := len(needle)
@@ -46,25 +42,103 @@ func searchVertical(needle []rune, haystack []string) int {
 	for c := 0; c < numColumns; c++ {
 
 		//--- Downward
-		numMatches := 0
-		for r := 0; r < numRows; r++ {
-			if haystack[r][c] == byte(needle[numMatches]) {
-				numMatches++
-				if numMatches == needleLength {
-					count++
-					numMatches = 0
+		for r := 0; r <= numRows-needleLength; r++ {
+			numMatches := 0
+			for offset := 0; offset < needleLength; offset++ {
+				if haystack[r+offset][c] == byte(needle[numMatches]) {
+					numMatches++
+					if numMatches == needleLength {
+						count++
+						numMatches = 0
+					}
 				}
 			}
 		}
 
 		//--- Upward
-		numMatches = 0
-		for r := numRows - 1; r >= 0; r-- {
-			if haystack[r][c] == byte(needle[numMatches]) {
-				numMatches++
-				if numMatches == needleLength {
-					count++
-					numMatches = 0
+		for r := numRows - 1; r >= needleLength-1; r-- {
+			numMatches := 0
+			for offset := 0; offset < needleLength; offset++ {
+				if haystack[r-offset][c] == byte(needle[numMatches]) {
+					numMatches++
+					if numMatches == needleLength {
+						count++
+						numMatches = 0
+					}
+				}
+			}
+		}
+	}
+
+	return count
+}
+
+func searchDiagonal(needle string, haystack []string) int {
+	numRows := len(haystack)
+	numColumns := len(haystack[0])
+	needleLength := len(needle)
+
+	count := 0
+
+	//--- Downward NW-SE
+	for r := 0; r <= numRows-needleLength; r++ {
+		for c := 0; c <= numColumns-needleLength; c++ {
+			numMatches := 0
+			for offset := 0; offset < needleLength; offset++ {
+				if haystack[r+offset][c+offset] == byte(needle[numMatches]) {
+					numMatches++
+					if numMatches == needleLength {
+						count++
+						numMatches = 0
+					}
+				}
+			}
+		}
+	}
+
+	//--- Downward NE-SW
+	for r := 0; r <= numRows-needleLength; r++ {
+		for c := numColumns - 1; c >= needleLength-1; c-- {
+			numMatches := 0
+			for offset := 0; offset < needleLength; offset++ {
+				if haystack[r+offset][c-offset] == byte(needle[numMatches]) {
+					numMatches++
+					if numMatches == needleLength {
+						count++
+						numMatches = 0
+					}
+				}
+			}
+		}
+	}
+
+	//--- Upward SW-NE
+	for r := numRows - 1; r >= needleLength-1; r-- {
+		for c := 0; c <= numColumns-needleLength; c++ {
+			numMatches := 0
+			for offset := 0; offset < needleLength; offset++ {
+				if haystack[r-offset][c+offset] == byte(needle[numMatches]) {
+					numMatches++
+					if numMatches == needleLength {
+						count++
+						numMatches = 0
+					}
+				}
+			}
+		}
+	}
+
+	//--- Upward SE-NW
+	for r := numRows - 1; r >= needleLength-1; r-- {
+		for c := numColumns - 1; c >= needleLength-1; c-- {
+			numMatches := 0
+			for offset := 0; offset < needleLength; offset++ {
+				if haystack[r-offset][c-offset] == byte(needle[numMatches]) {
+					numMatches++
+					if numMatches == needleLength {
+						count++
+						numMatches = 0
+					}
 				}
 			}
 		}
@@ -82,8 +156,9 @@ func SearchForWord(needle string, haystack []string) int {
 		count += searchHorizontal(rneedle, line)
 	}
 
-	needleArray := stringToArray(needle)
-	count += searchVertical(needleArray, haystack)
+	count += searchVertical(needle, haystack)
+
+	count += searchDiagonal(needle, haystack)
 
 	return count
 }
--- 04/search/wordsearch_test.go
+++ 04/search/wordsearch_test.go
@@ -45,10 +45,24 @@ func TestSearchVertical(t *testing.T) {
 			".A.",
 			".S.",
 		}, 1},
+		{"1 time down with false start", []string{
+			".X.",
+			".X.",
+			".M.",
+			".A.",
+			".S.",
+		}, 1},
 		{"1 time up", []string{
 			"..S",
 			"..A",
 			"..M",
+			"..X",
+		}, 1},
+		{"1 time up with false start", []string{
+			"..S",
+			"..A",
+			"..M",
+			"..X",
 			"..X",
 		}, 1},
 		{"2 times down", []string{
@@ -76,6 +90,128 @@ func TestSearchVertical(t *testing.T) {
 		}, 2},
 	}
 
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			num := SearchForWord(needle, testCase.input)
+			if num != testCase.expected {
+				t.Errorf("%v: expected %v but got %v", testCase.name, testCase.expected, num)
+			}
+		})
+	}
+}
+
+func TestSearchDiagonal(t *testing.T) {
+	needle := "XMAS"
+	testCases := []struct {
+		name     string
+		input    []string
+		expected int
+	}{
+		{"0 times", []string{
+			"MXMah",
+			"XAXbg",
+			"SXMcf",
+			"MSXde",
+		}, 0},
+		{"1 time down NW-SE", []string{
+			"X...",
+			".M..",
+			"..A.",
+			"...S",
+		}, 1},
+		{"1 time down NE-SW", []string{
+			"...X",
+			"..M.",
+			".A..",
+			"S...",
+		}, 1},
+		{"1 time up SW-NE", []string{
+			"...S",
+			"..A.",
+			".M..",
+			"X...",
+		}, 1},
+		{"1 time up SE-NW", []string{
+			"S...",
+			".A..",
+			"..M.",
+			"...X",
+		}, 1},
+		{"2 times down", []string{
+			"X....",
+			".M...",
+			"X.A..",
+			".M.S.",
+			"..A..",
+			"...S.",
+		}, 2},
+		{"down and up", []string{
+			"X..S",
+			".MA.",
+			".MA.",
+			"X..S",
+		}, 2},
+		{"overlap", []string{
+			"X......",
+			".M.....",
+			"..A....",
+			"...S...",
+			"....A..",
+			".....M.",
+			"......X",
+		}, 2},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			num := SearchForWord(needle, testCase.input)
+			if num != testCase.expected {
+				t.Errorf("%v: expected %v but got %v", testCase.name, testCase.expected, num)
+			}
+		})
+	}
+}
+
+func TestFullGrid(t *testing.T) {
+	needle := "XMAS"
+	testCases := []struct {
+		name     string
+		input    []string
+		expected int
+	}{
+		{"full grid", []string{
+			"MMMSXXMASM",
+			"MSAMXMSMSA",
+			"AMXSXMAAMM",
+			"MSAMASMSMX",
+			"XMASAMXAMM",
+			"XXAMMXXAMA",
+			"SMSMSASXSS",
+			"SAXAMASAAA",
+			"MAMMMXMMMM",
+			"MXMXAXMASX",
+		}, 18},
+		{"full grid with dots", []string{
+			//0123456789
+			"....XXMAS.", // 0
+			".SAMXMS...", // 1
+			"...S..A...", // 2
+			"..A.A.MS.X", // 3
+			"XMASAMX.MM", // 4
+			"X.....XA.A", // 5
+			"S.S.S.S.SS", // 6
+			".A.A.A.A.A", // 7
+			"..M.M.M.MM", // 8
+			".X.X.XMASX", // 9
+		}, 18},
+	}
+
+	// Horizontal : 5
+	// Vertical   : 3 [99-69U, 39-69D, 46-16U]
+	// Diagonal  NWSE : 1
+	// Diagonal  NESW : 1
+	// Diagonal  SWNE : 4
+	// Diagonal  SENW : 4
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			num := SearchForWord(needle, testCase.input)
